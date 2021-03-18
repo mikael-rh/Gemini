ff301d711296af153f6960518c7098971a74a8a2
Thomas Skarshaug <thomas.skarshaug@zeabuz.com>
2021-03-18 15:26:26 +0100

    Implement simple simulation_client

---
+++ Clients/python/simulation/simulation_client.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+import grpc
+
+import simulation_pb2
+import simulation_pb2_grpc
+
+def run():
+    channel = grpc.insecure_channel('192.168.86.98:12346')
+    stub = simulation_pb2_grpc.SimulationStub(channel)
+
+    #result = stub.SetStartTime(simulation_pb2.SetStartTimeRequest(time=0.0))
+
+    result = stub.Render(simulation_pb2_.RenderRequest(render=True))
+
+
+if __name__ == "__main__":
+    run()
