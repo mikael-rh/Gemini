7af8fa41763529aeb53860f332587f57ed6f9cb6
Thomas Skarshaug <thomas.skarshaug@zeabuz.com>
2021-03-18 14:18:32 +0100

    Add CameraTrigger script

---
+++ Gemini-Unity/Assets/Gemini/Scripts/Misc/CameraTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraTrigger : MonoBehaviour
+{
+    public Camera camera;
+    public RenderTexture renderTexture;
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera.targetTexture = renderTexture;
+        camera.enabled = false;
+    }
+
+
+    void Update()
+    {
+        Debug.Log("Updating");
+       
+        if (Time.time > 10.0)
+        {
+            Debug.Log("Rendering frame");
+            camera.Render();
+        }
+    }
+
+}
