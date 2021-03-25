61ca38a5f239e5c8541851d599326a60ef3d2b4c
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2021-03-25 20:00:45 +0100

    Experimenting with Controller implementations

---
+++ Gemini-Unity/Assets/Gemini/Scripts/Networking/Services/Simulation/ControllerBase.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google.Protobuf;
+using Grpc.Core;
+using GeminiOSPInterface;
+
+namespace Gemini.Networking.Services
+{
+    public class ControllerBase : MonoBehaviour
+    {
+        public string host = "localhost";
+
+        private int _port = 12346;
+
+        public int Port
+        {
+            get => _port;
+        }
+        protected static bool isServerinInitialized = false;
+
+        public GameObject[] boatPrefabs;
+
+        private SimulationServiceImpl serviceImpl;
+        protected Server server = new Server
+                {
+                    Services = {},
+                    Ports = {}
+                };
+    
+
+        void Awake()
+        {
+            if (!isServerinInitialized) 
+            {
+                server = new Server
+                {
+                    Services = {Simulation.BindService(serviceImpl)},
+                    Ports = { new ServerPort(host, _port, ServerCredentials.Insecure) }
+                };
+
+                Debug.Log("Simulation server listening on port: " + _port);
+                server.Start();
+                isServerinInitialized = true;
+            }
+        }
+
+        /*
+        void Start()
+        {
+            if (!isServerinInitialized) 
+            {
+                server = new Server
+                {
+                    Services = { Simulation.BindService(serviceImpl) },
+                    Ports = { new ServerPort(host, _port, ServerCredentials.Insecure) }
+                };
+
+                Debug.Log("Simulation server listening on port: " + _port);
+                server.Start();
+                isServerinInitialized = true;
+            }
+        }
+        */
+    }
+}
