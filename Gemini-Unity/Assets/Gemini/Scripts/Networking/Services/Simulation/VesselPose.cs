fbf102f70fe237e0810cd7710adb94e3ba75c482
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2021-03-25 19:38:05 +0100

    Rename Pose to VesselPose

---
+++ Gemini-Unity/Assets/Gemini/Scripts/Networking/Services/Simulation/VesselPose.cs
@@ -0,0 +1,37 @@
+﻿namespace Gemini.Networking.Services
+{
+    public class VesselPose
+    {
+        private float _north = 0.0f;
+        private float _east = 0.0f;
+        private float _heading = 0.0f;
+
+        public float North
+        {
+            get => _north;
+            set => _north = value;
+                    
+        }
+
+        public float East
+        {
+            get => _east;
+            set => _east = value;
+                    
+        }
+
+        public float Heading
+        {
+            get => _heading;
+            set => _heading = value;
+                    
+        }
+
+        public VesselPose(float north, float east, float heading)
+        {
+            this._north = north;
+            this._east = east;
+            this._heading = heading;
+        }
+    }
+}
