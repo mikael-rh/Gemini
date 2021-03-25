19859736b539b32117b776137b0026e7aad89c4d
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2021-03-25 14:44:33 +0100

    Simulation service implemented using Generics and delegate

---
+++ Gemini-Unity/Assets/Gemini/Scripts/Networking/Services/Executor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gemini.Core;
+using System.Threading;
+using System;
+
+public static class Executor
+{
+    public static T Execute<T, V, U>(V func, U request)
+        where T : new()
+        where V : Delegate 
+    {
+        T response = new T();
+        ManualResetEvent signalEvent = new ManualResetEvent(false);
+
+        ThreadManager.ExecuteOnMainThread(() =>
+        {
+            response = (T)func.DynamicInvoke(request);
+            signalEvent.Set();
+        });
+
+        // Wait for the event to be triggered from the action, signaling that the action is finished
+        // This is required becaue we are reading and depending on state from a resource running on the
+        // Unity main thread.
+        signalEvent.WaitOne();
+        signalEvent.Close();
+        return response;
+    }
+}
--- Gemini-Unity/Assets/Gemini/Scripts/Networking/Services/Simulation/SimulationServiceImpl.cs
+++ Gemini-Unity/Assets/Gemini/Scripts/Networking/Services/Simulation/SimulationServiceImpl.cs
@@ -1,142 +1,35 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Grpc.Core;
-using System.Threading;
 using GeminiOSPInterface;
-using Gemini.Core;
-using System;
 
 namespace Gemini.Networking.Services {
+    
+    public interface ISimulationService
+    {
+        StepResponse DoStep(StepRequest request);
+    }
+
     public class SimulationServiceImpl : Simulation.SimulationBase
     {
-
-        delegate void Something (StepRequest request);
-
-        delegate StepResponse SomethingElse (StepRequest request);
+        delegate T Del<T, V>(V request);
 
         private SimulationController _simulationController;
-        private GameObject[] _boats;
-
-        public SimulationServiceImpl(SimulationController simulationController, GameObject[] boats)
+        public SimulationServiceImpl(SimulationController simulationController)
         {
             _simulationController = simulationController;
-            _boats = boats;
         }
 
         public override async Task<StepResponse> DoStep(
             StepRequest request, ServerCallContext context)
         {
-            // Create the event that triggers when the execution of the action is finished.
-            ManualResetEvent signalEvent = new ManualResetEvent(false);
-
-            List<Pose> poses = new List<Pose>();
-
-            // Converting the poses that to a simple List<Pose> instead of ReapeatedField
-            foreach (GeminiOSPInterface.Pose pose in request.VesselPoses)
-            {
-                poses.Add(new Pose(pose.North, pose.East, pose.Heading));
-            }
-
-            float time = request.Time;
-            float stepSize = request.StepSize;
-
-            ThreadManager.ExecuteOnMainThread(() =>
-            {
-                for (int boatIdx = 0; boatIdx < _boats.Length; boatIdx++)
-                {
-                    if (boatIdx < _boats.Length)
-                    {
-                        _boats[boatIdx].transform.position = new Vector3(poses[boatIdx].East,0,poses[boatIdx].North);
-                        float Heading = poses[boatIdx].Heading;
-                        Quaternion QuaternionRot = Quaternion.AngleAxis(Heading, new Vector3(0, 1, 0));
-                        _boats[boatIdx].transform.rotation = QuaternionRot;
-                    }
-                }
-                signalEvent.Set();
-            });
-
-            // Wait for the event to be triggered from the action, signaling that the action is finished
-            // This is required becaue we are reading and depending on state from a resource running on the
-            // Unity main thread.
-            signalEvent.WaitOne();
-            signalEvent.Close();
-
-            
-            Execute(_simulationController.ExecuteStepRequest, request);
-
-            ExecuteWithResult(_simulationController.ExcuteStepRequestWithResult, request);
-
-            return await Task.FromResult(new StepResponse
-            {
-                Success = true,
-            });
+            //return await Task.FromResult(Executor.Execute<StepResponse, Del<StepResponse, StepRequest>, StepRequest>(_simulationController.ExcuteStepRequest, request)); 
+            return await Task.FromResult(Executor.Execute<StepResponse, Del<StepResponse, StepRequest>, StepRequest>(_simulationController.DoStep, request)); 
         }
-
 
         public override async Task<SetStartTimeResponse> SetStartTime(
             SetStartTimeRequest request, ServerCallContext context)
         {
-            // Create the event that triggers when the execution of the action is finished.
-            ManualResetEvent signalEvent = new ManualResetEvent(false);
-            var startTime = request.Time;
-
-            ThreadManager.ExecuteOnMainThread(() =>
-            {
-                GameObject.FindObjectOfType<Camera>();
-                signalEvent.Set();
-
-            });
-
-            // Wait for the event to be triggered from the action, signaling that the action is finished
-            // This is required becaue we are reading and depending on state from a resource running on the
-            // Unity main thread.
-            signalEvent.WaitOne();
-            signalEvent.Close();
-
-            // Sets the step size in simulation controller
-            return await Task.FromResult(new SetStartTimeResponse
-            {
-                Success = true,
-            });
+            return await Task.FromResult(Executor.Execute<SetStartTimeResponse, Del<SetStartTimeResponse, SetStartTimeRequest>, SetStartTimeRequest>(_simulationController.ExcecuteSetStartTime, request));
         }
-
-        private void Execute(Something action, StepRequest request)
-        {
-            ManualResetEvent signalEvent = new ManualResetEvent(false);
-
-            ThreadManager.ExecuteOnMainThread(() =>
-            {
-                action.Invoke(request);
-                signalEvent.Set();
-            });
-
-            // Wait for the event to be triggered from the action, signaling that the action is finished
-            // This is required becaue we are reading and depending on state from a resource running on the
-            // Unity main thread.
-            signalEvent.WaitOne();
-            signalEvent.Close();
-        }
-
-        private StepResponse ExecuteWithResult(SomethingElse func, StepRequest request)
-        {
-            StepResponse response = new StepResponse();
-            ManualResetEvent signalEvent = new ManualResetEvent(false);
-
-            ThreadManager.ExecuteOnMainThread(() =>
-            {
-                response = func.Invoke(request);
-                signalEvent.Set();
-            });
-
-            // Wait for the event to be triggered from the action, signaling that the action is finished
-            // This is required becaue we are reading and depending on state from a resource running on the
-            // Unity main thread.
-            signalEvent.WaitOne();
-            signalEvent.Close();
-            //return func(request);
-            return response;
-        }
-
     }
 }
